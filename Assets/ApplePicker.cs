7a4bfb0157c07422b8a80485cc1c5c0d5a1a5428
PJ Rosa <demotednyk@gmail.com>
2016-02-18 19:50:43 -0500

    cleaned code

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -24,22 +24,16 @@ public class ApplePicker : MonoBehaviour
         }
     }
     public void AppleDestroyed()
-    { // 2
-      //// Destroy all of the falling Apples
+    {
         GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");// 3
         foreach (GameObject tGO in tAppleArray)
         {
             Destroy(tGO);
         }
-        //// Destroy one of the Baskets
-        // Get the index of the last Basket in basketList
         int basketIndex = basketList.Count - 1;
-        // Get a reference to that Basket GameObject
         GameObject tBasketGO = basketList[basketIndex];
-        // Remove the Basket from the List and destroy the GameObject
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGO);
-        // Restart the game, which doesn't affect HighScore.Score
         if (basketList.Count == 0)
         {
             Application.LoadLevel("_Scene_0");
--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,19 +3,13 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour
 {
-    // Prefab for instantiating apples
     public GameObject applePrefab;
-    // Speed at which the AppleTree moves in meters/second
     public float speed = 1f;
-    // Distance where AppleTree turns around
     public float leftAndRightEdge = 10f;
-    // Chance that the AppleTree will change directions
     public float chanceToChangeDirections = 0.1f;
-    // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrops = 1f;
     void Start()
     {
-        // Dropping apples every second
         InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
     }
     void DropApple()
@@ -25,11 +19,9 @@ public class AppleTree : MonoBehaviour
     }
     void Update()
     {
-        // Basic Movement
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
-        // Changing Direction
         if (pos.x < -leftAndRightEdge)
         {
             speed = Mathf. Abs(speed); // Move right
@@ -41,10 +33,9 @@ public class AppleTree : MonoBehaviour
     }
     void FixedUpdate()
     {
-        // Changing Direction Randomly
         if (Random.value < chanceToChangeDirections)
         {
-            speed *= -1; // Change direction
+            speed *= -1;
         }
     }
 }
